a16d88330b5b8f481a42550c8061d964d30a5e91
romas <89295077542@mail.ru>
2026-05-08 12:43:15 +0000

    Fix conanfile to force C compiler settings

--- sprint1/problems/sync_server/solution/conanfile.py
+++ sprint1/problems/sync_server/solution/conanfile.py
@@ -1,13 +1,20 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain
 
 class HelloConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeDeps"
     requires = "boost/1.83.0"
 
     def layout(self):
         cmake_layout(self)
-        
+    
     def generate(self):
-        pass
+        tc = CMakeToolchain(self)
+        # Принудительно задаём компиляторы
+        tc.variables["CMAKE_C_COMPILER"] = "/usr/bin/g++"
+        tc.variables["CMAKE_C_COMPILER_WORKS"] = "ON"
+        tc.variables["CMAKE_CXX_COMPILER"] = "/usr/bin/g++"
+        tc.variables["CMAKE_CXX_COMPILER_WORKS"] = "ON"
+        tc.generate()
