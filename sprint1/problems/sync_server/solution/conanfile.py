4faaf44116307bc8303079328da81c44205da6c8
romas <89295077542@mail.ru>
2026-05-08 12:23:38 +0000

    Fresh re-add of sync_server solution with all files

--- sprint1/problems/sync_server/solution/conanfile.py
+++ sprint1/problems/sync_server/solution/conanfile.py
@@ -1,9 +1,8 @@
 from conan import ConanFile
-
+from conan.tools.cmake import cmake_layout
 class HelloConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
     requires = "boost/1.83.0"
-
-    def generate(self):
-        pass
+    def layout(self):
+        cmake_layout(self)
