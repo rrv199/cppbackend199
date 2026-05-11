90cc6b4fed3d3a0bdb35d90c41f3d6bb43dc9f0b
romas <89295077542@mail.ru>
2026-05-11 14:40:16 +0000

    Complete join_game: add player and player_manager, fix build

--- sprint2/problems/join_game/solution/player_manager.h
+++ sprint2/problems/join_game/solution/player_manager.h
@@ -1,3 +1,37 @@
+#pragma once
+
+#include "player.h"
+#include <unordered_map>
+#include <random>
+#include <iomanip>
+#include <sstream>
+#include <memory>
+#include <string>
+
+class PlayerManager {
+public:
+    static PlayerManager& Instance() {
+        static PlayerManager instance;
+        return instance;
+    }
+
+    std::pair<int, std::string> CreatePlayer(const std::string& name, const std::string& map_id) {
+        int id = next_id_++;
+        std::string token = GenerateToken();
+        auto player = std::make_shared<Player>(id, name, token, map_id);
+        players_[id] = player;
+        token_to_player_[token] = player;
+        map_players_[map_id][id] = player;
+        return {id, token};
+    }
+
+    std::shared_ptr<Player> GetPlayerByToken(const std::string& token) const {
+        auto it = token_to_player_.find(token);
+        if (it != token_to_player_.end()) {
+            return it->second;
+        }
+        return nullptr;
+    }
 
     const std::unordered_map<int, std::shared_ptr<Player>>& GetPlayersOnMap(const std::string& map_id) const {
         static const std::unordered_map<int, std::shared_ptr<Player>> empty;
@@ -7,3 +41,22 @@
         }
         return empty;
     }
+
+private:
+    PlayerManager() : rng_(std::random_device{}()) {}
+
+    std::string GenerateToken() {
+        std::uniform_int_distribution<int> dist(0, 15);
+        std::ostringstream oss;
+        for (int i = 0; i < 32; ++i) {
+            oss << std::hex << dist(rng_);
+        }
+        return oss.str();
+    }
+
+    int next_id_ = 0;
+    std::mt19937 rng_;
+    std::unordered_map<int, std::shared_ptr<Player>> players_;
+    std::unordered_map<std::string, std::shared_ptr<Player>> token_to_player_;
+    std::unordered_map<std::string, std::unordered_map<int, std::shared_ptr<Player>>> map_players_;
+};
