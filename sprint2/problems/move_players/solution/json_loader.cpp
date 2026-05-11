c9443160f43a816dfe00507ea1207d446595614c
romas <89295077542@mail.ru>
2026-05-11 18:03:17 +0000

    Fix offset loading: convert double to int

--- sprint2/problems/move_players/solution/json_loader.cpp
+++ sprint2/problems/move_players/solution/json_loader.cpp
@@ -102,8 +102,8 @@ model::Game LoadGame(const std::filesystem::path& json_path) {
                 model::Offset offset;
                 pos.x = office_obj.at("x").as_int64();
                 pos.y = office_obj.at("y").as_int64();
-                offset.dx = office_obj.at("offsetX").as_int64();
-                offset.dy = office_obj.at("offsetY").as_int64();
+                offset.dx = static_cast<int>(office_obj.at("offsetX").as_double());
+                offset.dy = static_cast<int>(office_obj.at("offsetY").as_double());
                 map.AddOffice(model::Office(
                     model::Office::Id{std::move(office_id)},
                     pos, offset));
