43508e1b7d1ad7750327dee14540eb6a79d4dbae
romas <89295077542@mail.ru>
2026-05-16 14:27:56 +0000

    revert: remove database creation, only create table

--- sprint4/problems/db_of_books/solution/src/main.cpp
+++ sprint4/problems/db_of_books/solution/src/main.cpp
@@ -9,34 +9,10 @@ using json = nlohmann::json;
 
 class BookManager {
 public:
-    BookManager(const std::string& conn_string) {
-        // Извлекаем имя базы данных
-        std::string dbname;
-        std::string admin_conn_string;
-        
-        size_t last_slash = conn_string.rfind('/');
-        if (last_slash != std::string::npos) {
-            dbname = conn_string.substr(last_slash + 1);
-            admin_conn_string = conn_string.substr(0, last_slash + 1) + "postgres";
-        } else {
-            dbname = "postgres";
-            admin_conn_string = conn_string;
-        }
-        
-        // Пытаемся создать базу данных
-        try {
-            pqxx::connection admin_conn(admin_conn_string);
-            pqxx::work w(admin_conn);
-            w.exec("CREATE DATABASE " + dbname);
-            w.commit();
-        } catch (...) {
-            // База данных уже существует или не удалось создать
-        }
-        
-        // Подключаемся к нужной базе данных
-        conn_ = std::make_shared<pqxx::connection>(conn_string);
+    BookManager(const std::string& conn_string) 
+        : conn_(std::make_shared<pqxx::connection>(conn_string)) {
         prepareStatements();
-        createTable();
+        createTableIfNotExists();
     }
     
     bool addBook(const std::string& title, const std::string& author, 
@@ -52,7 +28,7 @@ public:
             
             w.commit();
             return true;
-        } catch (...) {
+        } catch (const std::exception& e) {
             return false;
         }
     }
@@ -79,14 +55,14 @@ public:
                 }
                 result.push_back(book);
             }
-        } catch (...) {
-            // Игнорируем
+        } catch (const std::exception& e) {
+            // Возвращаем пустой массив
         }
         return result;
     }
     
 private:
-    void createTable() {
+    void createTableIfNotExists() {
         try {
             pqxx::work w(*conn_);
             w.exec(
@@ -97,8 +73,8 @@ private:
                 "year integer NOT NULL, "
                 "isbn char(13) UNIQUE)");
             w.commit();
-        } catch (...) {
-            // Игнорируем
+        } catch (const std::exception& e) {
+            // Таблица уже существует или другая ошибка
         }
     }
     
